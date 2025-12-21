ea9be01d1b1a9ad6b00f049f2f4a110019a6e0e7
thahseen.sadiqas@gmail.com <thahseen.sadiqas@gmail.com>
2025-12-21 22:16:21 +0530

    port 5000

--- server/index.js
+++ server/index.js
@@ -57,6 +57,7 @@ app.post("/sendemail", (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
